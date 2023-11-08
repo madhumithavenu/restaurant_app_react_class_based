30571d973ac612b62e54f7a0916dd0c24f93cd56
Madhumitha <madhumithavenu03@gmail.com>
2023-11-08 15:57:17 +0530

    Adding Username and password to login form

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -5,6 +5,17 @@ export default class Login extends Component {
         return(
             <div>
                 <h1>Login Component</h1>
+                <form onSubmit={null}>
+                    <div className="wrapper">
+                        
+                        Username: <input type="text" name="username" id="username" required /> <br />
+
+                        
+                        Password: <input type="password" name="password" id="password" /> <br />
+
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
             </div>
         )
     }
