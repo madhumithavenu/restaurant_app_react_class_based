607958e6ac980613111108f7b4c070b6255d301c
Madhumitha <madhumithavenu03@gmail.com>
2023-11-10 15:20:22 +0530

    Creating restaurant app

--- frontend/src/components/restaurant/Restaurant.jsx
+++ frontend/src/components/restaurant/Restaurant.jsx
@@ -1,87 +1,113 @@
 // import { useState, useEffect } from 'react';
+import { Component } from 'react';
 import './Restaurant.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 export default class Restaurant extends Component {
-    // const [menuData, setMenuData] = useState({
-    //     flag: false,
-    //     foodData: null,
-    //     drinkData: null
-    // });
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuData: {
+                flag: false,
+                foodData: null,
+                drinkData: null,
+            },
+        };
+    }
 
-    // async function getData() {
-    //     let backend_url = 'http://localhost:3200/data';
-    //     let response = await fetch(backend_url);
-    //     let responseData = await response.json();
-    //     setMenuData({
-    //         flag: true,
-    //         foodData: responseData[0],
-    //         drinkData: responseData[1]
-    //     });
-    // }
+    async componentDidMount() {
+        await this.getData();
+    }
 
-    // useEffect(() => {
-    //     getData();
-    // }, []);
-    render(){
-    return (
-        <div className='pri'>
-            <div>
+    async getData() {
+        let backend_url = 'http://localhost:3200/data';
+
+        try {
+            let response = await fetch(backend_url);
+
+            if (response.ok) {
+                let responseData = await response.json();
+                this.setState({
+                    menuData: {
+                        flag: true,
+                        foodData: responseData[0],
+                        drinkData: responseData[1],
+                    },
+                });
+                console.log(responseData);
+            } else {
+                console.error('Failed to fetch data');
+            }
+        } catch (error) {
+            console.error('API call error:', error);
+        }
+    }
+
+    render() {
+
+        const { flag, foodData, drinkData } = this.state.menuData;
+
+        return (
+            <div className='pri1'>
                 <h1> Food Menu</h1>
 
-                <div className='food-container'>
-                    {
-                        menuData.flag ? menuData.foodData.map(item => {
-                            return <Card className='food-item' style={{ "width": '18rem' }} key={item.foodName}>
-                                <Card.Img variant="top" src={'images/food/' + item.foodName + '.jpg'} />
-                                <Card.Body className={(item.category === 'veg') ? 'veg' : 'non-veg'}>
-                                    <Card.Title>{item.foodName} : {item.price} </Card.Title>
-                                    <Card.Text>
-                                        {item.foodName} is an amazing Dish. You should try it.
-                                    </Card.Text>
-                                    <Card.Text>
-                                    Category:
-                                    <img src={(item.category === 'veg') ?  'images/veg.png' :  'images/non-veg.png'}
-                                        height="30px" width="30px" />
-                                    </Card.Text>
-                                    <Button variant="primary">Buy Now</Button>
- 
-                                </Card.Body>
-                            </Card>
-                        }) : <h1>Loading</h1>
-                    }
-
-                </div>
+                {flag ? (
+                    <ul>
+                        <div className='food-container'>
+                            {foodData.map((item) => (
+                                <Card className='food-item' style={{ width: '18rem' }} key={item.foodName}>
+                                    <Card.Img variant="top" src={'images/food/' + item.foodName + '.jpg'} />
+                                    <Card.Body className={(item.category === 'veg') ? 'veg' : 'non-veg'}>
+                                        <Card.Title>{item.foodName} : {item.price}</Card.Title>
+                                        <Card.Text>
+                                            {item.foodName} is an amazing dish. You should try it.
+                                        </Card.Text>
+                                        <div>
+                                            Category: <img
+                                                src={(item.category === 'veg') ? 'images/veg.png' : 'images/non-veg.png'}
+                                                height="30px" width="30px"
+                                                alt={(item.category === 'veg') ? 'Veg' : 'Non-Veg'}
+                                            />
+                                        </div>
+                                        <Button variant="primary">Buy Now</Button>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                        </div>
+                    </ul>
+                ) : (
+                    <h1>Loading</h1>
+                )
+                }
 
                 <h1> Drink Menu</h1>
 
-                <div className='drink-container'>
-                    {
-                        menuData.flag ? menuData.drinkData.map(item => {
-                            return <Card className='drink-item' style={{ "width": '18rem' }} key={item.drinkName}>
-                                <Card.Img variant="top" src={'images/drink/' + item.drinkName + '.jpg'} />
-                                <Card.Body className={(item.category === 'cocktail') ? 'cocktail' : 'mocktail'}>
-                                    <Card.Title>{item.drinkName} : {item.price} </Card.Title>
-                                    <Card.Text>
-                                        {item.drinkName} is an amazing Drink. You should try it.
-                                    </Card.Text>
-                                    <Card.Text>
-                                        Category: {item.category}
-                                    </Card.Text>
+                {
+                    flag ? (
+                        <ul>
+                            {drinkData.map((item) => (
+                                <Card className='drink-item' style={{ width: '18rem' }} key={item.drinkName}>
+                                    <Card.Img variant='top' src={'images/drink/' + item.drinkName + '.jpg'} />
+                                    <Card.Body className={item.category === 'mocktail' ? 'mocktail' : 'cocktail'}>
+                                        <Card.Title>{item.drinkName} : {item.price}</Card.Title>
+                                        <Card.Text>
+                                            {item.drinkName} is an amazing Drink. You should try it.
+                                        </Card.Text>
+                                        <Card.Text>
+                                            Category: {item.category}
+                                        </Card.Text>
+                                        <Button variant='primary'>Buy Now</Button>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                        </ul>
+                    ) : (
+                        <h1>Loading</h1>
+                    )
+                }
 
-                                    <Button variant="primary">Buy Now</Button>
-                                </Card.Body>
-                            </Card>
-                        }) : <h1>Loading</h1>
-                    }
-
-                </div>
-            </div>
-
-
-        </div>
-    );
+            </div >
+        );
+    }
 }
-}
-
