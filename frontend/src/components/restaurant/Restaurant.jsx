28e81f9d063c4a8f8bb00fc5073e1cc2a9f5c038
Madhumitha <madhumithavenu03@gmail.com>
2023-11-10 15:45:04 +0530

    completed restaurant app

--- frontend/src/components/restaurant/Restaurant.jsx
+++ frontend/src/components/restaurant/Restaurant.jsx
@@ -49,65 +49,61 @@ export default class Restaurant extends Component {
         const { flag, foodData, drinkData } = this.state.menuData;
 
         return (
-            <div className='pri1'>
-                <h1> Food Menu</h1>
-
-                {flag ? (
-                    <ul>
-                        <div className='food-container'>
-                            {foodData.map((item) => (
-                                <Card className='food-item' style={{ width: '18rem' }} key={item.foodName}>
+            <div className='pri'>
+                <div>
+                    <h1> Food Menu</h1>
+    
+                    <div className='food-container'>
+                        {
+                            flag ? foodData.map(item => {
+                                return <Card className='food-item' style={{ "width": '18rem' }} key={item.foodName}>
                                     <Card.Img variant="top" src={'images/food/' + item.foodName + '.jpg'} />
                                     <Card.Body className={(item.category === 'veg') ? 'veg' : 'non-veg'}>
-                                        <Card.Title>{item.foodName} : {item.price}</Card.Title>
+                                        <Card.Title>{item.foodName} : {item.price} </Card.Title>
                                         <Card.Text>
-                                            {item.foodName} is an amazing dish. You should try it.
+                                            {item.foodName} is an amazing Dish. You should try it.
                                         </Card.Text>
-                                        <div>
-                                            Category: <img
-                                                src={(item.category === 'veg') ? 'images/veg.png' : 'images/non-veg.png'}
-                                                height="30px" width="30px"
-                                                alt={(item.category === 'veg') ? 'Veg' : 'Non-Veg'}
-                                            />
-                                        </div>
+                                        <Card.Text>
+                                        Category:
+                                        <img src={(item.category === 'veg') ?  'images/veg.png' :  'images/non-veg.png'}
+                                            height="30px" width="30px" />
+                                        </Card.Text>
                                         <Button variant="primary">Buy Now</Button>
+     
                                     </Card.Body>
                                 </Card>
-                            ))}
-                        </div>
-                    </ul>
-                ) : (
-                    <h1>Loading</h1>
-                )
-                }
-
-                <h1> Drink Menu</h1>
-
-                {
-                    flag ? (
-                        <ul>
-                            {drinkData.map((item) => (
-                                <Card className='drink-item' style={{ width: '18rem' }} key={item.drinkName}>
-                                    <Card.Img variant='top' src={'images/drink/' + item.drinkName + '.jpg'} />
-                                    <Card.Body className={item.category === 'mocktail' ? 'mocktail' : 'cocktail'}>
-                                        <Card.Title>{item.drinkName} : {item.price}</Card.Title>
+                            }) : <h1>Loading</h1>
+                        }
+    
+                    </div>
+    
+                    <h1> Drink Menu</h1>
+    
+                    <div className='drink-container'>
+                        {
+                            flag ? drinkData.map(item => {
+                                return <Card className='drink-item' style={{ "width": '18rem' }} key={item.drinkName}>
+                                    <Card.Img variant="top" src={'images/drink/' + item.drinkName + '.jpg'} />
+                                    <Card.Body className={(item.category === 'cocktail') ? 'cocktail' : 'mocktail'}>
+                                        <Card.Title>{item.drinkName} : {item.price} </Card.Title>
                                         <Card.Text>
                                             {item.drinkName} is an amazing Drink. You should try it.
                                         </Card.Text>
                                         <Card.Text>
                                             Category: {item.category}
                                         </Card.Text>
-                                        <Button variant='primary'>Buy Now</Button>
+    
+                                        <Button variant="primary">Buy Now</Button>
                                     </Card.Body>
                                 </Card>
-                            ))}
-                        </ul>
-                    ) : (
-                        <h1>Loading</h1>
-                    )
-                }
-
-            </div >
+                            }) : <h1>Loading</h1>
+                        }
+    
+                    </div>
+                </div>
+    
+    
+            </div>
         );
     }
 }
